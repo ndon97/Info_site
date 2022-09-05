aac8ca7568fe3fcc5b4ba5955276e4cb0c4d0591
Nick Don <nick.don.1102@gmail.com>
2022-09-05 21:13:34 +1000

    removing useless lines and cleaning up code

--- index.js
+++ index.js
@@ -30,9 +30,6 @@ function MainContent() {
               able to fit in with the cool kids!</li>
               <li>I'm more likely to get a job as a developer
               if I know React</li>
-              <li>It's intresting learning react its a completely new way of writing code</li>
-              <li>There are so much things i love about react</li>
-          </ol>
       </div>
   )
 }
